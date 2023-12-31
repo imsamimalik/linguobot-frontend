56d7a750af31a3d38dc19d8c8eb63db4d4d9410e
imsamimalik <imsamimalikk@gmail.com>
2024-01-01 01:14:10 +0500

    initial
    Signed-off-by: imsamimalik <imsamimalikk@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import FaceLandmarkCanvas from "@/components/FaceLandmarkCanvas";
+
+const App = () => {
+    return (
+        <div className="bg-gradient-to-r from-purple-500 to-blue-800 flex flex-col items-center min-h-screen px-2 pt-10 text-white">
+            <h1 className="md:text-4xl text-shadow mb-2 text-xl font-bold text-center">
+                Mediapie FaceLandmarker Demo
+            </h1>
+            <div className="flex justify-center w-full">
+                <FaceLandmarkCanvas />
+            </div>
+        </div>
+    );
+};
+export default App;
