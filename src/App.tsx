ed59736f15a99615ab98b048df96030cb37d7c33
imsamimalik <imsamimalikk@gmail.com>
2024-01-06 04:47:57 +0500

    bg
    Signed-off-by: imsamimalik <imsamimalikk@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import PoseLandmarkCanvas from "@/components/PoseLandmarkCanvas";
 
 const App = () => {
     return (
-        <div className="bg-gradient-to-r from-purple-500 to-blue-800 flex flex-col items-center min-h-screen px-2 pt-10 text-white">
+        <div className="bg-slate-400 flex flex-col items-center min-h-screen px-2 pt-10 text-white">
             <h1 className="md:text-4xl text-shadow mb-2 text-xl font-bold text-center">
                 LinguoBot Demo
             </h1>
