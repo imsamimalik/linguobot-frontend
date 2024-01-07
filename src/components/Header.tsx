350ae89f13c2d3730b1f05f1483388791905ad7e
imsamimalik <imsamimalikk@gmail.com>
2024-01-08 01:57:17 +0500

    remove dark mode from Header
    Signed-off-by: imsamimalik <imsamimalikk@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,20 +7,20 @@ const Header = () => {
     );
 
     return (
-        <nav className="dark:bg-gray-900 start-0 dark:border-gray-600 w-full bg-white border-b border-gray-200">
+        <nav className="start-0 w-full bg-white border-b border-gray-200">
             <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
                 <Link
                     to="/"
                     className="rtl:space-x-reverse flex items-center space-x-3"
                 >
-                    <span className="whitespace-nowrap dark:text-white self-center text-2xl font-semibold">
+                    <span className="whitespace-nowrap self-center text-2xl font-semibold">
                         LinguoBot
                     </span>
                 </Link>
                 <div className="md:order-2 md:space-x-0 rtl:space-x-reverse flex space-x-3">
                     <button
                         type="button"
-                        className="hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg"
+                        className="hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg"
                         onClick={toggleAvatarCreatorView}
                     >
                         Customize
@@ -30,11 +30,11 @@ const Header = () => {
                     className="md:flex md:w-auto md:order-1 items-center justify-between hidden w-full"
                     id="navbar-sticky"
                 >
-                    <ul className="md:p-0 bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg">
+                    <ul className="md:p-0 bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg">
                         <li>
                             <Link
                                 to="/"
-                                className="md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 block px-3 py-2 text-white bg-blue-700 rounded"
+                                className="md:bg-transparent md:text-blue-700 md:p-0 block px-3 py-2 text-white bg-blue-700 rounded"
                                 aria-current="page"
                             >
                                 Skeleton View
@@ -44,7 +44,7 @@ const Header = () => {
                         <li>
                             <Link
                                 to="/avatar"
-                                className="md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 block px-3 py-2 text-white bg-blue-700 rounded"
+                                className="md:bg-transparent md:text-blue-700 md:p-0 block px-3 py-2 text-white bg-blue-700 rounded"
                                 aria-current="page"
                             >
                                 Avatar View
